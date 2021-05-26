764819bffa7c92cc7ac18c23b0ccc25861489fa4
canburaks <canburak@msn.com>
2021-05-26 07:27:50 +0300

    Mailer eklendi ve test edildi.

---
+++ build/config/database.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Env_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Core/Env"));
+const databaseConfig = {
+    connection: Env_1.default.get('DB_CONNECTION'),
+    connections: {
+        sqlite: {
+            client: 'sqlite',
+            connection: {
+                filename: 'database/db.sqlite',
+            },
+            migrations: {
+                naturalSort: true,
+            },
+            useNullAsDefault: true,
+            healthCheck: false,
+            debug: false,
+        },
+    },
+};
+exports.default = databaseConfig;
+//# sourceMappingURL=database.js.map
