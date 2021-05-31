ed7d635f960374589324a7446ce7dc89e55cf297
canburaks <canburak@msn.com>
2021-05-31 03:09:12 +0300

    auto-commit

--- start/kernel.ts
+++ start/kernel.ts
@@ -21,6 +21,7 @@ import Server from '@ioc:Adonis/Core/Server'
 |
 */
 Server.middleware.register([() => import('@ioc:Adonis/Core/BodyParser')])
+// Open the builder directly
 
 /*
 |--------------------------------------------------------------------------
